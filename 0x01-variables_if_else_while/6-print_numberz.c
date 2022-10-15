e28d974c6f3cc9f331d016ff8229773d206e9439
Paul Aderoju <paul.adeboye@yahoo.com>
2022-10-14 18:39:38 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Numberz
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number = 0;
+
+	for (; number < 10; number++)
+		putchar(number);
+
+	putchar('\n');
+
+	return (0);
+
+}

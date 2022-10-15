e62d287ad1b73cabf6ceff8aa8e673b74fa55b43
Paul Aderoju <paul.adeboye@yahoo.com>
2022-10-14 20:05:20 -0700

    10 again

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,8 +18,8 @@ int main(void)
 			putchar((num1 % 10) + '0');
 			putchar((num2 % 10) + '0');
 
-			if (num1 != 9 && num2 != 9)
-				continue;
+			if (num1 == 8 && num2 == 9)
+				break;
 
 			putchar(',');
 			putchar(' ');

f11f3f0e03c81231472de12f9597c3231fc55946
Paul Aderoju <paul.adeboye@yahoo.com>
2022-10-14 18:47:14 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Printing alphabets in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char leta;
+
+	for (leta = 'z'; leta <= 'a'; leta--)
+		putchar(leta);
+
+	putchar('\n');
+
+	return (0);
+
+}

1e599ab50eb3a8fa0adad77329b7abb3f56a7b39
Paul Aderoju <paul.adeboye@yahoo.com>
2022-10-21 17:19:43 -0700

    10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle, using the character #
+ * @size: the size of the triangle.
+ */
+
+void print_triangle(int size)
+{
+	int hash, i;
+
+	if (size > 0)
+	{
+		for (hash = 1; hash <= size; hash++)
+		{
+			for (i = size - hash; i > 0; i--)
+				_putchar(' ');
+
+			for (i = 0; i < hash; i++)
+				_putchar('#');
+
+			if (hash == size)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+
+}
